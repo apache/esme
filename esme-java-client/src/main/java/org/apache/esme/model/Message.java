af775007b7b8270dc10ed498f23531b69cd8ea3f
Darren Hague <dhague@apache.org>
2009-02-05 21:38:44 +0000

    getMessages is now implemented.  resources/props/test.properties allows you to specify the token & API URL to use for JUnit test.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/esme/trunk@741305 13f79535-47bb-0310-9956-ffa450edef68

--- esme-java-client/src/main/java/org/apache/esme/model/Message.java
+++ esme-java-client/src/main/java/org/apache/esme/model/Message.java
@@ -13,9 +13,41 @@ package org.apache.esme.model;
  * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
  */
 public class Message {
-	private String text = "";
+//        <message id="1134" source="web" date="...">
+//                <author image="..." name="..." id="9"/>
+//                <body>
+//                        Woohoo - got through the rest of the slides quite quickly. I can go home now.
+//                </body>
+//                <tags/>
+//        </message>
+
+    private String messageId;
+	private String source = "Java API";
+    private String date;
+
+    private String authorId;
+    private String authorName;
+    private String authorImage;
+
+    private String body = "";
 	private String[] tags = {};
-	private String via = "Java API";
+
+    public String toString()
+    {
+        StringBuffer tagsStr = new StringBuffer();
+        tagsStr.append("[");
+        for (int i=0; i<tags.length; i++)
+        {
+            if (i!=0) tagsStr.append(", ");
+            tagsStr.append(tags[i]);
+        }
+        tagsStr.append("]");
+
+        return "messageId="+messageId+", source="+source+", date="+date+", authorId="+
+                authorId+", authorName="+authorName+", authorImage="+authorImage+", body="+
+                body+", tags="+tagsStr.toString();
+    }
+    
 	/**
 	 * @return
 	 */
@@ -26,15 +58,15 @@ public class Message {
 	/**
 	 * @return
 	 */
-	public String getText() {
-		return text;
+	public String getBody() {
+		return body;
 	}
 
 	/**
 	 * @return
 	 */
-	public String getVia() {
-		return via;
+	public String getSource() {
+		return source;
 	}
 
 	/**
@@ -47,15 +79,55 @@ public class Message {
 	/**
 	 * @param string
 	 */
-	public void setText(String string) {
-		text = string;
+	public void setBody(String string) {
+		body = string;
 	}
 
 	/**
 	 * @param string
 	 */
-	public void setVia(String string) {
-		via = string;
+	public void setSource(String string) {
+		source = string;
 	}
+
+        public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorImage() {
+        return authorImage;
+    }
+
+    public void setAuthorImage(String authorImage) {
+        this.authorImage = authorImage;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
 
 }
