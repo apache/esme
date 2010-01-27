8bde7b837d0da665850f6118d5e4a78fd2cbf51d
Anne Kathrine Petteroe <akpetteroe@apache.org>
2010-01-27 17:27:18 +0000

    ESME-47 Some Licensing Nits
    git-svn-id: https://svn.apache.org/repos/asf/incubator/esme/trunk/server@903744 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,8 +1,6 @@
 package bootstrap.liftweb
 
 /**
- * Copyright 2008-2009 WorldWide Conferencing, LLC
- *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
--- src/main/scala/org/apache/esme/model/ExtSession.scala
+++ src/main/scala/org/apache/esme/model/ExtSession.scala
@@ -1,7 +1,5 @@
 package org.apache.esme.model
 /**
- * Copyright 2008-2009 WorldWide Conferencing, LLC
- * 
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
