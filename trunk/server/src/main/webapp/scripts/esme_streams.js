6ba697827f18462dc11488ec794d8d10e345b774
Richard Hirsch <rhirsch@apache.org>
2010-05-07 07:45:48 +0000

    [ESME-100] Finish web UI Daily hour
    git-svn-id: https://svn.apache.org/repos/asf/incubator/esme@942020 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/server/src/main/webapp/scripts/esme_streams.js
+++ trunk/server/src/main/webapp/scripts/esme_streams.js
@@ -7,7 +7,13 @@
  "License"); you may not use this file except in compliance   *
  with the License.  You may obtain a copy of the License at   *
                                                               *
-   http://www.apache.org/licenses/LICENSE-2.0                 *
+   http://www.apache.org/licenses/LICENSE-2.0     
+   
+   $("#prob_test").bind('DOMSubtreeModified', function() 
+    { 
+       alert ('hi'); 
+    } 
+);             *
                                                               *
  Unless required by applicable law or agreed to in writing,   *
  software distributed under the License is distributed on an  *
@@ -18,9 +24,4 @@
 */
 
 
-
-
-$(function(){
-    $(".tipelement").tipTip({maxWidth: "auto", edgeOffset: 10});
-});
-   
+$("#prob_test").change( function() { alert("Hello"); } );
