b53a5decf46b66ad7cf2206483a10c625a72e8bf
Richard Hirsch <rhirsch@apache.org>
2010-08-27 13:54:35 +0000

    fixed licensing issues on js files based on rat results
    git-svn-id: https://svn.apache.org/repos/asf/incubator/esme/trunk/server@990152 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/scala/org/apache/esme/liftwebext/SHtml.scala
+++ src/main/scala/org/apache/esme/liftwebext/SHtml.scala
@@ -1,4 +1,23 @@
-package org.apache.esme.liftwebext
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+ package org.apache.esme.liftwebext
 
 import _root_.net.liftweb._
 import http._
@@ -13,13 +32,6 @@ import JE._
 import JsCmds._
 import _root_.scala.xml._
 
-/**
- * Created by IntelliJ IDEA.
- * User: imtiaz
- * Date: Jun 27, 2010
- * Time: 11:40:19 AM
- * To change this template use File | Settings | File Templates.
- */
 
 object SHtml {
   import net.liftweb.http.js.JsCmds.JsCrVar
