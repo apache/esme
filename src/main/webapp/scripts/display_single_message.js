ad5a2195b49846fbbe33193480b659c8df16302f
Richard Hirsch <rhirsch@apache.org>
2010-09-06 15:10:53 +0000

    [ESME-265] Generic Task to prepare for 1.1 release Fixing UI problem on first screen with layout
    git-svn-id: https://svn.apache.org/repos/asf/incubator/esme/trunk/server@993066 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/webapp/scripts/display_single_message.js
+++ src/main/webapp/scripts/display_single_message.js
@@ -114,11 +114,12 @@ function displayMessages(msgArray, elementId)
       .attr('alt',msgAuthor.firstname + ' ' + msgAuthor.lastname);
 
 
-      newMsg.find('#body').html(msgBody);
-      newMsg.find('#supp_data').text(msgPool + " " + msgDateStr  + " " +  msgReason  + " " +   msgSource);
-      //newMsg.find('#source').text(msgSource);
-      //newMsg.find('#reason').text(msgReason);
-      //newMsg.find('#when').text(msgDateStr);
+      newMsg.find('#msgbody').html(msgBody);
+      if (msgReason)
+         newMsg.find('#supp_data').text(msgPool + " " + msgDateStr  + " " +  msgReason );
+      else {
+      	 newMsg.find('#supp_data').text(msgPool + " " + msgDateStr  + " via "  +   msgSource);
+      }
       var id = cometMsg.id;
       var resendButton = newMsg.find('#resend');
       if (cometResent) {
