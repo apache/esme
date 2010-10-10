914acf5287f0e6e059df0dcab828f4962fe7f816
Ethan Jewett <esjewett@apache.org>
2010-10-10 18:07:49 +0000

    Merge tagandconversationfollow branch into trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/esme/trunk/server@1006328 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/scala/org/apache/esme/actor/ConvDistributor.scala
+++ src/main/scala/org/apache/esme/actor/ConvDistributor.scala
@@ -1,5 +1,3 @@
-package org.apache.esme.model
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
@@ -19,24 +17,30 @@ package org.apache.esme.model
  * under the License.
  */
 
-import net.liftweb._
-import mapper._
-import util._
-import common._
-import Helpers._
+package org.apache.esme.actor  
 
-object MessageTag extends MessageTag with LongKeyedMetaMapper[MessageTag] {
+import net.liftweb._  
+import actor._        
 
-}
+import org.apache.esme._
+import model.{Message, User, ConvFollowReason}
 
-class MessageTag extends LongKeyedMapper[MessageTag] {
-  def getSingleton = MessageTag // what's the "meta" server
-  def primaryKeyField = id
+object ConvDistributor extends LiftActor {   
 
-  object id extends MappedLongIndex(this)
+  println("ConvDistributor started")
+                             
+  protected def messageHandler = {
+    case Distributor.NewMessage(msg) => {                                                            
+      Message.findMessages(List(msg.conversation)).values.toList.map( m => {  
+        m.followers.refresh.map( u => {                             
+          Distributor ! Distributor.AddMessageToMailbox(u.id, msg, ConvFollowReason(m.id)); 
+        })
+      })
+    }
+  } 
   
-  object message extends MappedLongForeignKey(this, Message)
-  object tag extends MappedLongForeignKey(this, Tag)
-  object sentTo extends MappedLongForeignKey(this, User)
-  object url extends MappedLongForeignKey(this, UrlStore)
+  def touch {
+  
+  }                   
+
 }
