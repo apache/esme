2d77da7cdffea4e77db465f84ee4057790e058ad
Ivanov Vladimir Viktorovich <lester@apache.org>
2011-04-06 11:23:38 +0000

    Completed ESME-341: Upgrade to Lift 2.3 version.
    git-svn-id: https://svn.apache.org/repos/asf/esme/trunk/server@1089404 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/scala/org/apache/esme/model/ExtSession.scala
+++ src/main/scala/org/apache/esme/model/ExtSession.scala
@@ -24,7 +24,7 @@ import mapper._
 import util._
 import common._
 
-object ExtSession extends ExtSession with MetaProtoExtendedSession[ExtSession] {
+object ExtSession extends ExtSession with LongKeyedMetaMapper[ExtSession] with MetaProtoExtendedSession[ExtSession] {
   override def dbTableName = "ext_session" // define the DB table name
 
   def logUserIdIn(uid: String): Unit = User.logUserIdIn(uid)
@@ -34,7 +34,7 @@ object ExtSession extends ExtSession with MetaProtoExtendedSession[ExtSession] {
   type UserType = User
 }
 
-class ExtSession extends ProtoExtendedSession[ExtSession] {
+class ExtSession extends LongKeyedMapper[ExtSession] with ProtoExtendedSession[ExtSession] {
   def getSingleton = ExtSession // what's the "meta" server
 
 }
